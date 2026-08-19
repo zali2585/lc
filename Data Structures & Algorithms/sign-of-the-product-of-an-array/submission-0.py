ca7767f7529f7be5a0c7c055a3a4333d65ac49c5
zali2585 <zyna.5000@gmail.com>
2026-08-18 18:25:38 -0700

    Add: sign-of-the-product-of-an-array - submission-0

---
+++ Data Structures & Algorithms/sign-of-the-product-of-an-array/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+        for n in nums:
+            product *= n
+        if product < 0:
+            return -1
+        elif product == 0:
+            return 0
+        else:
+            return 1
+    
+    
